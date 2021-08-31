974a4dab5f973e653c57275af5053ffc06ff84c6
Jekker600 <79650628+Jekker600@users.noreply.github.com>
2021-08-31 14:39:56 +0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-2"
 }
-data "aws_ami" "web" {
+data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
@@ -17,24 +17,37 @@ data "aws_ami" "web" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "web" {
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+data "aws_security_group" "ssh_enabled" {
+  name = "launch-wizard-1"
+}
+
+resource "aws_instance" "test1" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
+  key_name = "mk-rsa"
+  root_block_device {
+	volume_size = 10
+  }
+  
+  ebs_block_device {
+    device_name = "/dev/sdf"
+    volume_size = 5
+    volume_type = "gp2"
+    delete_on_termination = true
+  }
+  
+  disable_api_termination = true
 
   tags = {
-    Name = "HelloWorld"
+    Name = "TestInstance1"
+    Project = "Netology"
   }
 }
-data "aws_caller_identity" "current" {}
 
-output "account_id" {
-  value = data.aws_caller_identity.current.account_id
-}
-
-output "caller_arn" {
-  value = data.aws_caller_identity.current.arn
-}
-
-output "caller_user" {
-  value = data.aws_caller_identity.current.user_id
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = "${data.aws_security_group.ssh_enabled.id}"
+  network_interface_id = "${aws_instance.test1.primary_network_interface_id}"
 }
